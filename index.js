be7fc0ce93bd007201013b55f15e77813e952ae9
Ryan Silva <RyandotSilva@gmail.com>
2019-04-27 18:47:25 -0400

    Add get request handler

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const express = require('express');
 // Set up Express App
 const app = express();
 
+app.get('/api', (req, res) => {
+	console.log('GET request');
+	res.send({
+		name: 'Yoshi'
+	});
+});
+
 // Listen for requests
 app.listen(process.env.port || 4000, () => {
 	console.log('Now listening for requests');
