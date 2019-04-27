a52c946daa52ada31bc72c44023373ef10dd0516
Ryan Silva <RyandotSilva@gmail.com>
2019-04-27 18:38:00 -0400

    Set up listener for app

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const express = require('express');
 
 // Set up Express App
 const app = express();
+
+// Listen for requests
+app.listen(proccess.env.port || 4000, () => {
+	console.log('Now listening for requests');
+});
