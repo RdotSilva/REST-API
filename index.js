9ed7fcdf22219c77a0bccb9d2e2b26dc00248a4e
Ryan Silva <RyandotSilva@gmail.com>
2019-04-27 18:50:53 -0400

    Remove get request handler

--- index.js
+++ index.js
@@ -3,13 +3,6 @@ const express = require('express');
 // Set up Express App
 const app = express();
 
-app.get('/api', (req, res) => {
-	console.log('GET request');
-	res.send({
-		name: 'Yoshi'
-	});
-});
-
 // Listen for requests
 app.listen(process.env.port || 4000, () => {
 	console.log('Now listening for requests');
