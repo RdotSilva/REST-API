d09fa2b310a61899aa9d75197b665a198e555b5d
Ryan Silva <RyandotSilva@gmail.com>
2019-04-28 16:45:53 -0400

    Add name to NinjaSchema

---
+++ models/ninja.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create ninja Schema and model
+const NinjaSchema = new Schema({
+	name: {
+		type: String,
+		required: [true, 'Name field is required']
+	}
+});
