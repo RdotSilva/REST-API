3b58084ea78ef0348b6b464e2b7cd4d73387b17b
Ryan Silva <RyandotSilva@gmail.com>
2019-04-28 17:23:21 -0400

    Add status to error handling

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use('/api', routes);
 // Error handling middleware
 app.use((err, req, res, next) => {
 	// console.log(err);
-	res.send({ error: err.message });
+	res.status(422).send({ error: err.message });
 });
 
 // Listen for requests
